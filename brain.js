a3e7d6c071ec45f1fc02d453c72fef94185cd73c
hchiam <hchiam@users.noreply.github.com>
2019-06-02 22:58:17 -0400

    scan now / auto-scan / print QR code

--- brain.js
+++ brain.js
@@ -1,9 +1,11 @@
 let overlayOpacity = 0.75;
 let cameraId = 0; // default fallback
 let numberofCameras = 0; // default fallback
+let isScanOn = false;
 
 document.getElementById('clear-button').style.display = 'none';
 document.getElementById('try-another-camera').style.display = 'none';
+$('#print-qr-code').hide();
 
 let scanner = new Instascan.Scanner({
   video: document.getElementById('video')
@@ -12,7 +14,7 @@ let scanner = new Instascan.Scanner({
 scanner.addListener('scan', function (content) {
   console.log(content);
   var isOk = true; // content.startsWith('https://localhost/');
-  if (isOk) {
+  if (isScanOn && isOk) {
     document.getElementById('preview').src = content.replace(/^http:/,'https:');
     document.getElementById('preview').style.opacity = overlayOpacity;
     document.getElementById('placeholder').innerHTML = '';
@@ -21,6 +23,26 @@ scanner.addListener('scan', function (content) {
     // window.open(content);
   }
 });
+
+function scanNow() {
+  isScanOn = true;
+  document.getElementById('scan').style.background = 'blue';
+  document.getElementById('scan').style.color = 'white';
+  document.getElementById('scan').innerHTML = 'Scanning...';
+  let interval = 5000;
+  setTimeout(function() {
+    isScanOn = false;
+    document.getElementById('scan').style.background = 'yellow';
+    document.getElementById('scan').style.color = 'black';
+    document.getElementById('scan').innerHTML = 'Scan Now';
+  }, interval);
+}
+
+function autoScan() {
+  document.getElementById('auto-scan').style.background = 'blue';
+  alert('Warning: Auto-scan will use more battery.');
+  isScanOn = true;
+}
 
 useCamera();
 
@@ -57,4 +79,31 @@ function clearPreview() {
   document.getElementById('preview').style.opacity = 0;
   document.getElementById('placeholder').innerHTML = 'Show me a QR code';
   document.getElementById('clear-button').style.display = 'none';
+}
+
+function createQRCode() {
+  var data = $('#data').val();
+  $('#qr-code').empty();
+  if (data) {
+    $('#qr-code').qrcode(data);
+    $('#print-qr-code').show();
+  } else {
+    $('#print-qr-code').hide();
+  }
+};
+
+$('#data').keyup(createQRCode);
+$('#data').change(createQRCode);
+
+function printQRCode() {
+  let elemID = 'qr-code';
+  $('body > :not(#' + elemID + ')').hide();
+  $('#' + elemID).appendTo('body');
+  window.print();
+  $('#' + elemID).appendTo('#qr-code-container');
+  $('#scan').show();
+  $('#auto-scan').show();
+  $('#overlay').show();
+  $('#create-qr-code').show();
+  return true;
 }
